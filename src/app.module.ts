a4617c77f6af14259332518d075ba70f1881e3d2
minhoo03 <minhoo0333@gmail.com>
2021-08-04 00:00:43 +0900

    Start Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
+// 모듈
+// 모든 로직을 모아 앱 실행
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // 이러한 형태의 함수는 데코레이터라고 부른다.
 // *클래스에 함수 기능 추가한다. (클래스 위의 함수)
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
-
-// ctrl: url 가져온 후 함수 실행 (라우터)
