596963f27517667f9462c0b1f5acad5adfaa35e2
minhoo03 <minhoo0333@gmail.com>
2021-08-07 00:51:17 +0900

    Use spec.ts

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,10 @@
+// npm run test:watch
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
   let service: MoviesService;
-
+  // 테스트 하기 전 실행
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +13,13 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // 개별 테스트
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be 4', () => {
+    // 조건
+    expect(2+2).toEqual(4)
+  })
 });
